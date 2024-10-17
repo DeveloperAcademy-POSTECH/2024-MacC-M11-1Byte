e53def30ffd475a8b162b2cc37b9e85a1161da7d
Mars <108053426+SANGDOLEE@users.noreply.github.com>
2024-10-17 10:39:50 +0900

    Initial Commit

---
+++ OneByte/OneByteApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneByteApp.swift
+//  OneByte
+//
+//  Created by 이상도 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct OneByteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
