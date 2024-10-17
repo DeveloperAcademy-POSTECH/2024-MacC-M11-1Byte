eebdce46c4ec7ac9639a03d63da7ffc163338304
Trudy2645 <162314061+Trudy2645@users.noreply.github.com>
2024-10-17 11:43:28 +0900

    Feat: 탭바뷰 추가

---
+++ OneByte/TabBar/ObjectiveView.swift
@@ -0,0 +1,14 @@
+//
+//  ObjectiveView.swift
+//  OneByte
+//
+//  Created by 트루디 on 10/17/24.
+//
+
+import SwiftUI
+
+struct ObjectiveView: View {
+    var body: some View {
+        Text("목표 뷰")
+    }
+}
