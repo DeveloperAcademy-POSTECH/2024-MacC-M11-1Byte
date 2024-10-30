f6a876cbd0ca26d53c9cc7db9d4208941e5454b8
SANGDOLEE <xkdxh625@naver.com>
2024-10-30 14:40:34 +0900

    Feat: 프로토콜과 유스케이스 추가

---
+++ OneByte/Source/UseCases/Client/ClientUpdateService.swift
@@ -0,0 +1,24 @@
+//
+//  ClientUpdateService.swift
+//  OneByte
+//
+//  Created by 이상도 on 10/30/24.
+//
+
+import Foundation
+
+//class ClientUpdateService: UpdateGoalUseCase {
+//    func updateMainGoal(id: UUID, newTitle: String, isAchieved: Bool) -> MainGoal {
+//        <#code#>
+//    }
+//    
+//    func updateSubGoal(id: UUID, newTitle: String, isAchieved: Bool) -> SubGoal {
+//        <#code#>
+//    }
+//    
+//    func updateDetailGoal(id: UUID, newTitle: String, isAchieved: Bool) -> DetailGoal {
+//        <#code#>
+//    }
+//    
+//    
+//}
