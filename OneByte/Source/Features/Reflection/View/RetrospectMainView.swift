5118d891f88666664e2b9277ea4c7a6b3d7ef1c7
SANGDOLEE <xkdxh625@naver.com>
2024-11-04 15:54:39 +0900

    Feat: 회고 MainView 기본 구조 수정

--- OneByte/Source/Features/Reflection/View/RetrospectMainView.swift
+++ OneByte/Source/Features/Reflection/View/RetrospectMainView.swift
@@ -14,13 +14,68 @@ struct RetrospectMainView: View {
     var body: some View {
         NavigationStack(path: $navigationManager.path) {
             VStack {
+                HStack {
+                    Text("베로님은\n총 5개의 꽃을 피웠어요") // 나중에 데이터 값으로 수정
+                        .font(.title3)
+                        .bold()
+                    Spacer()
+                }
+                .padding()
+                
+                Image(systemName: "camera.macro.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 300)
+                
+                Text("다음 회고까지 D-13") // 나중에 날짜 데이터 값으로 수정
+                    .bold()
+                
                 Button {
                     navigationManager.push(to: .select)
                 } label: {
-                    Text("월 선택 후 화면이동")
+                    Text("회고 하기")
+                        .font(.system(size: 16))
+                }
+                .frame(width: 225, height: 38)
+                .background(.blue)
+                .foregroundStyle(.white)
+                .cornerRadius(8)
+                
+                HStack {
+                    Button {
+                        navigationManager.push(to: .total)
+                    } label: {
+                        Text("회고 모아보기")
+                            .foregroundStyle(.black)
+                            .bold()
+                    }
+                    Image(systemName: "chevron.right")
+                        .bold()
+                    
+                    Spacer()
+                }
+                .padding()
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(0..<11) {_ in
+                            VStack {
+                                // 나중에 실제 회고 완료 데이터 값으로 수정
+                                Image(systemName: "camera.macro.circle.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                Text("월")
+                                    .foregroundColor(.black)
+                            }
+                        }
+                    }
+                    .frame(maxHeight: .infinity)
+                    .padding()
                 }
             }
-            .navigationTitle(Date().YearString)
+            .navigationTitle("\(Date().YearString) 회고")
             .navigationDestination(for: PathType.self) { pathType in
                 pathType.NavigatingView()
             }
