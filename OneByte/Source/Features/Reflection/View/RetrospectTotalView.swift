271c0904c71f71bc64cae69e2f883d48dcefbd21
SANGDOLEE <xkdxh625@naver.com>
2024-11-04 16:03:37 +0900

    Feat: 회고 TotalView 생성

--- OneByte/Source/Features/Reflection/View/RetrospectTotalView.swift
+++ OneByte/Source/Features/Reflection/View/RetrospectTotalView.swift
@@ -11,8 +11,38 @@ struct RetrospectTotalView: View {
     
     @Environment(NavigationManager.self) var navigationManager
     
+    let items = Array(1...12) // 뷰 테스트용 임시 변수
+    
+    let columns = [ // VGrid 3열
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(items, id: \.self) { item in
+                        VStack {
+                            // 나중에 Cell을 만들어서 회고 데이터 삽입
+                            Text("\(item)월의 꽃")
+                                .font(.headline)
+                                .frame(maxWidth: .infinity, minHeight: 131)
+                                .background(Color.blue.opacity(0.3))
+                                .cornerRadius(8)
+                                .padding(5)
+                        }
+                    }
+                }
+                .padding()
+            }
+        }
+        .navigationTitle("회고 모아보기")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationDestination(for: PathType.self) { pathType in
+            pathType.NavigatingView()
+        }
     }
 }
 
