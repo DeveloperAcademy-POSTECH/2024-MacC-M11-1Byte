253f09615a03dabc2d7e0b880be26eab09ee3633
Mars <108053426+SANGDOLEE@users.noreply.github.com>
2024-11-09 12:02:23 +0900

    Revert "[Trudy/Feat/#23] 구조 조정 및 안쓰는 파일 삭제"

--- OneByte/Source/Model/GoalModels.swift
+++ OneByte/Source/Model/GoalModels.swift
@@ -4,54 +4,49 @@
 //
 //  Created by 이상도 on 10/29/24.
 //
-import SwiftData
+
 import SwiftUI
+import SwiftData
 
 @Model
 class MainGoal {
-    var id: Int
-    var title: String
-    var goalYear: Int
-    var subGoals: [SubGoal]
-    
-    init(id: Int, title: String, goalYear: Int, subGoals: [SubGoal]) {
+    @Attribute(.unique) var id: UUID
+    var title: String = ""
+    var isAchieved: Bool = false
+    @Relationship var subGoals: [SubGoal] = []
+
+    init(id: UUID = UUID(), title: String = "", isAchieved: Bool = false) {
         self.id = id
         self.title = title
-        self.goalYear = goalYear
-        self.subGoals = subGoals
+        self.isAchieved = isAchieved
     }
 }
 
 @Model
 class SubGoal {
-    var id: Int
-    var title: String
-    var memo: String
-    var mainGoalId: Int  // MainGoal의 ID로 연결
-    var detailGoals: [DetailGoal]
-    
-    init(id: Int, title: String, memo: String, mainGoalId: Int, detailGoals: [DetailGoal]) {
+    @Attribute(.unique) var id: UUID
+    var title: String = ""
+    var isAchieved: Bool = false
+    @Relationship var detailGoals: [DetailGoal] = []
+    @Relationship(inverse: \MainGoal.subGoals) var mainGoal: MainGoal?
+
+    init(id: UUID = UUID(), title: String = "", isAchieved: Bool = false) {
         self.id = id
         self.title = title
-        self.memo = memo
-        self.mainGoalId = mainGoalId
-        self.detailGoals = detailGoals
+        self.isAchieved = isAchieved
     }
 }
 
 @Model
 class DetailGoal {
-    var id: Int
-    var title: String
-    var memo: String
-    var isAchieved: Bool
-    var subGoalId: Int  // SubGoal의 ID로 연결
+    @Attribute(.unique) var id: UUID
+    var title: String = ""
+    var isAchieved: Bool = false
+    @Relationship(inverse: \SubGoal.detailGoals) var subGoal: SubGoal?
 
-    init(id: Int, title: String, memo: String, isAchieved: Bool, subGoalId: Int) {
+    init(id: UUID = UUID(), title: String = "", isAchieved: Bool = false) {
         self.id = id
         self.title = title
-        self.memo = memo
         self.isAchieved = isAchieved
-        self.subGoalId = subGoalId
     }
 }
