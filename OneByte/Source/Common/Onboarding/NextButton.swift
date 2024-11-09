e8ddd475e2c8101209fb74c8070dced275169e59
SANGDOLEE <xkdxh625@naver.com>
2024-11-09 17:13:57 +0900

    Feat: 온보딩 Common 버튼 생성

---
+++ OneByte/Source/Common/Onboarding/NextButton.swift
@@ -0,0 +1,33 @@
+//
+//  OnboardingWideButtonView.swift
+//  OneByte
+//
+//  Created by 이상도 on 11/9/24.
+//
+
+import SwiftUI
+
+struct NextButton<Content: View>: View {
+    
+    let label: Content
+    let action: () -> Void
+    
+    init(action: @escaping () -> Void, @ViewBuilder label: () -> Content) {
+        self.label = label()
+        self.action = action
+    }
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            label
+                .font(.system(size: 18))
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 53)
+                .background(Color(hex: "636363"))
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+        }
+    }
+}
---
+++ OneByte/Source/Common/Onboarding/PassButton.swift
@@ -0,0 +1,38 @@
+//
+//  OnboardingShortButtonView.swift
+//  OneByte
+//
+//  Created by 이상도 on 11/9/24.
+//
+
+import SwiftUI
+
+struct PassButton<Content: View>: View {
+    
+    let label: Content
+    let action: () -> Void
+    
+    init(action: @escaping () -> Void, @ViewBuilder label: () -> Content) {
+        self.label = label()
+        self.action = action
+    }
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            label
+                .font(.system(size: 18))
+                .foregroundStyle(Color(hex: "636363"))
+                .frame(maxWidth: .infinity)
+                .frame(height: 53)
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+    
+        }
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color(hex: "636363"), lineWidth: 2)
+        )
+    }
+}
