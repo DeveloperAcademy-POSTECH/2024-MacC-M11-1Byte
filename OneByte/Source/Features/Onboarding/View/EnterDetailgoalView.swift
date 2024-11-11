63537f25bfa79dc8d079cef82d19e8f58f03fc07
SANGDOLEE <xkdxh625@naver.com>
2024-11-11 16:57:26 +0900

    Feat: 특정 빈 화면 터치시 Keyboard가 내려가지 않는 문제 해결

--- OneByte/Source/Features/Onboarding/View/EnterDetailgoalView.swift
+++ OneByte/Source/Features/Onboarding/View/EnterDetailgoalView.swift
@@ -187,6 +187,7 @@ struct EnterDetailgoalView: View {
             // EnterSubgoalView에서 사용자가 입력한 Subgoal중 id 1번 값을 찾아 담음
             targetSubGoal = subGoals.first(where: { $0.id == 1 })
         }
+        .contentShape(Rectangle())
         .onTapGesture {
             UIApplication.shared.endEditing() // 빈 화면 터치 시 키보드 숨기기
         }
--- OneByte/Source/Features/Onboarding/View/EnterMaingoalView.swift
+++ OneByte/Source/Features/Onboarding/View/EnterMaingoalView.swift
@@ -126,6 +126,7 @@ struct EnterMaingoalView: View {
             }
             .padding()
         }
+        .contentShape(Rectangle())
         .onTapGesture {
             UIApplication.shared.endEditing() // 빈 화면 터치 시 키보드 숨기기
         }
