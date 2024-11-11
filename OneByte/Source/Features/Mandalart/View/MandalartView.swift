9dfec5014c54729e8495ced06ebe62b8d9df7c0f
Trudy2645 <162314061+Trudy2645@users.noreply.github.com>
2024-11-12 01:32:22 +0900

    Feat & Design: 여러가지 색상 및 이미지 추가 MandalartView 하이파이대로 수정(첫 화면)

--- OneByte/Source/Features/Mandalart/View/MandalartView.swift
+++ OneByte/Source/Features/Mandalart/View/MandalartView.swift
@@ -43,33 +43,104 @@ struct OuterGridView: View {
     @Binding var isPresented: Bool
     @Binding var subGoal: SubGoal?
     @Binding var mainGoal: MainGoal? // mainGoal을 @Binding으로 사용
-
+    
+    @State var mainIsPresented: Bool = false
     private let outerColumns = Array(repeating: GridItem(.flexible()), count: 3)
 
     var body: some View {
-        if let selectedMainGoal = mainGoal {
-            let sortedSubGoals = selectedMainGoal.subGoals.sorted(by: { $0.id < $1.id }) // 정렬된 SubGoals 배열
+        VStack(alignment: .leading) {
+            // 앱 로고
+            Image("appLogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 22)
+                .foregroundStyle(Color.my9E9E9E)
+                .padding(.top, 20)
             
-            LazyVGrid(columns: outerColumns, spacing: 10) {
-                ForEach(0..<9, id: \.self) { index in
-                    if index == 4 {
-                        MainGoalCell(subGoal: $subGoal, mainGoal: $mainGoal, isPresented: $isPresented)
-                    } else {
-                        let subGoalIndex = index < 4 ? index : index - 1
-                        if subGoalIndex < sortedSubGoals.count {
-                            SubGoalCell(isPresented: $isPresented, selectedSubGoal: Binding(
-                                get: { sortedSubGoals[subGoalIndex] },
-                                set: { _ in }
-                            ))
+            Spacer()
+            
+            // 다라 & comment
+            HStack() {
+                Image("Dara5")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 65)
+                
+                ZStack{
+                    Image("comment")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 55)
+                    Text("한 걸을씩 가다 보면 \n어느새 큰 변화를 느낄 거예요!")
+                        .font(.system(size: 14))
+                }
+            }
+            
+            ZStack {
+                // 배경색과 모서리 둥글게 처리
+                Color.my538F53
+                    .cornerRadius(10)
+                
+                HStack {
+                    // 좌측 텍스트
+                    Text("나의 목표")
+                        .foregroundStyle(Color.myD5F3D1)
+                        .font(.Pretendard.Medium.size16)
+                    
+                    Spacer()
+                    
+                    // 중간 텍스트
+                    Text("많은 것을 도전하는 한 해 보내기")
+                        .foregroundStyle(.white)
+                        .font(.Pretendard.Bold.size16)
+                    
+                    Spacer()
+                    
+                    // 점 세 개 버튼
+                    Button(action: {
+                        mainIsPresented = true
+                    }) {
+                        Image(systemName: "ellipsis")
+                            .foregroundColor(.white)
+                            .rotationEffect(.degrees(90))
+                    }
+                    .sheet(isPresented: $mainIsPresented) {
+                        MainGoalsheetView(mainGoal: $mainGoal, isPresented: $mainIsPresented)
+                            .presentationDragIndicator(.visible)
+                            .presentationDetents([.height(244/852 * UIScreen.main.bounds.height)])
+                    }
+                }
+                .padding()
+            }
+            .frame(height: 43/852 * UIScreen.main.bounds.height)
+            .padding(.vertical)
+            
+            // 만다라트 그리드
+            if let selectedMainGoal = mainGoal {
+                let sortedSubGoals = selectedMainGoal.subGoals.sorted(by: { $0.id < $1.id }) // 정렬된 SubGoals 배열
+                
+                LazyVGrid(columns: outerColumns, spacing: 3) {
+                    ForEach(0..<9, id: \.self) { index in
+                        if index == 4 {
+                            MainGoalCell(subGoal: $subGoal, mainGoal: $mainGoal, isPresented: $isPresented)
                         } else {
-                            EmptyView()
+                            let subGoalIndex = index < 4 ? index : index - 1
+                            if subGoalIndex < sortedSubGoals.count {
+                                SubGoalCell(isPresented: $isPresented, selectedSubGoal: Binding(
+                                    get: { sortedSubGoals[subGoalIndex] },
+                                    set: { _ in }
+                                ))
+                            } else {
+                                EmptyView()
+                            }
                         }
                     }
                 }
+            } else {
+                Text("찾을 수 없습니다.")
             }
-            .padding()
-        } else {
-            Text("찾을 수 없습니다.")
+            Spacer()
         }
+        .padding(.horizontal, 20/393 * UIScreen.main.bounds.width)
     }
 }
