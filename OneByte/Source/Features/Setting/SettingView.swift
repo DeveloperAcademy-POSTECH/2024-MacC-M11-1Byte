801c0ffacbed14f8fc466d23146afa9034e34fbb
SANGDOLEE <xkdxh625@naver.com>
2024-11-23 23:48:32 +0900

    Refactor: 세팅뷰 안쓰는 항목 제거

--- OneByte/Source/Features/Setting/SettingView.swift
+++ OneByte/Source/Features/Setting/SettingView.swift
@@ -32,10 +32,10 @@ struct SettingView: View {
                         Divider()
                             .foregroundStyle(.clear)
                         
-                        AchievedRow(isAppearAchieved: $viewModel.isAppearAchieved)
+//                        AchievedRow(isAppearAchieved: $viewModel.isAppearAchieved)
                         
-                        Divider()
-                            .foregroundStyle(Color.myF0E8DF)
+//                        Divider()
+//                            .foregroundStyle(Color.myF0E8DF)
                         
                         Button {
                             viewModel.openAppSettings()
@@ -52,34 +52,34 @@ struct SettingView: View {
                         Divider()
                             .foregroundStyle(Color.myF0E8DF)
                         
-                        NavigationLink(destination: Text("폰트 선택")) {
-                            FontRow()
-                                .foregroundStyle(.black)
-                        }
-                        Divider()
-                            .foregroundStyle(.clear)
+//                        NavigationLink(destination: Text("폰트 선택")) {
+//                            FontRow()
+//                                .foregroundStyle(.black)
+//                        }
+//                        Divider()
+//                            .foregroundStyle(.clear)
                     }
                     .background(.white)
                     
-                    VStack {
-                        Divider()
-                            .foregroundStyle(.clear)
-                        
-                        NavigationLink(destination: Text("개인정보 처리 방침")) {
-                            PrivacyPolicyRow()
-                                .foregroundStyle(.black)
-                        }
-                        Divider()
-                            .foregroundStyle(Color.myF0E8DF)
-                        
-                        NavigationLink(destination: Text("이용 약관")) {
-                            TermsRow()
-                                .foregroundStyle(.black)
-                        }
-                        Divider()
-                            .foregroundStyle(.clear)
-                    }
-                    .background(.white)
+//                    VStack {
+//                        Divider()
+//                            .foregroundStyle(.clear)
+//                        
+//                        NavigationLink(destination: Text("개인정보 처리 방침")) {
+//                            PrivacyPolicyRow()
+//                                .foregroundStyle(.black)
+//                        }
+//                        Divider()
+//                            .foregroundStyle(Color.myF0E8DF)
+//                        
+//                        NavigationLink(destination: Text("이용 약관")) {
+//                            TermsRow()
+//                                .foregroundStyle(.black)
+//                        }
+//                        Divider()
+//                            .foregroundStyle(.clear)
+//                    }
+//                    .background(.white)
                     
                     HStack {
                         Text("Version \(viewModel.appVersion)") // 앱 버전정보
