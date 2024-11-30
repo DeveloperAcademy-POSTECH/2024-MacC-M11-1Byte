954ddefb9db27b491de22995eca838f1c94fffec
SANGDOLEE <xkdxh625@naver.com>
2024-11-30 17:16:48 +0900

    Fix: 온보딩 ExplainPages 텍스트 및 이미지 변경

--- OneByte/Source/Model/OnboardingExplain.swift
+++ OneByte/Source/Model/OnboardingExplain.swift
@@ -15,13 +15,13 @@ enum OnboardingExplain: CaseIterable {
     var onboardingTitle: String {
         switch self {
         case .first:
-            return "안녕하세요\n주간 루틴 관리 메이트\n만부기예요."
+            return "여러분의 루틴 관리를\n도와드릴게요"
         case .second:
-            return "달성하고 싶은 목표를\n루틴으로 관리해 보세요"
+            return "목표와 루틴을 설정해서\n클로버를 심을 수 있어요"
         case .third:
             return "주간 루틴을 잘 지키고 있는지\n한눈에 확인할 수 있어요"
         case .fourth:
-            return "성취도에 따라 매주 일요일 자정\n주간 클로버가 수집됩니다"
+            return "한 주를 마무리하며\n성취를 확인할 수 있어요"
         case .fifth:
             return "설정 시간에 맞춰\n알림을 보내드릴게요"
         }
@@ -30,13 +30,13 @@ enum OnboardingExplain: CaseIterable {
     var onboardingSubTitle: String {
         switch self {
         case .first:
-            return ""
+            return "지금부터 앱에 대한 소개를 시작해볼게요!"
         case .second:
             return "최대 4개의 목표와\n목표당 최대 3개의 루틴을 작성할 수 있어요"
         case .third:
-            return "주간 루틴을 실천할 때마다\n클로버의 색상이 진해지는 것을 확인할 수 있어요"
+            return "루틴을 하나씩 완료할 때마다\n클로버의 색상이 점점 진해져요"
         case .fourth:
-            return "일주일중 1개라도 실천하면 초록 클로버\n모든 루틴을 성취하면 황금클로버를 수집할 수 있어요"
+            return "매주 일요일 자정에 지난주의 성취에 따라\n클로버를 획득할 수 있어요"
         case .fifth:
             return "알림을 통해 잊지 않고 루틴을 실천해보세요"
         }
