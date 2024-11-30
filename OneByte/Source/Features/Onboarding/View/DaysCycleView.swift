a01522fc2e3faf853aacdfbe13a542e6e451c331
SANGDOLEE <xkdxh625@naver.com>
2024-12-01 01:47:07 +0900

    Refactor: DaysCycleView 리팩토링

--- OneByte/Source/Features/Onboarding/View/DaysCycleView.swift
+++ OneByte/Source/Features/Onboarding/View/DaysCycleView.swift
@@ -11,27 +11,9 @@ import SwiftData
 struct DaysCycleView: View {
     
     @Environment(NavigationManager.self) var navigationManager
-    @Environment(\.modelContext) private var modelContext
-    
-    @State var viewModel = RoutineCycleViewModel(updateService: UpdateService(mainGoals: [], subGoals: [], detailGoals: []))
-    
     @Query private var subGoals: [SubGoal]
     @Query private var detailGoals: [DetailGoal]
-    
-    @State private var targetSubGoal: SubGoal? // id가 1인 SubGoal 저장변수
-    @State private var targetDetailGoal: DetailGoal? // id가 1인 SubGoal 저장변수
-    
-    @State private var achieveGoal = 0
-    @State private var alertMon: Bool = false
-    @State private var alertTue: Bool = false
-    @State private var alertWed: Bool = false
-    @State private var alertThu: Bool = false
-    @State private var alertFri: Bool = false
-    @State private var alertSat: Bool = false
-    @State private var alertSun: Bool = false
-    
-    @State var selectedTime = ""
-    var routineTimes = ["아침","점심","저녁","자기 전","자율"]
+    @State var viewModel = RoutineCycleViewModel(updateService: UpdateService(mainGoals: [], subGoals: [], detailGoals: []))
     
     var nowOnboard: Onboarding = .daysCycle
     
@@ -56,160 +38,21 @@ struct DaysCycleView: View {
                     .frame(height: 86)
                     .overlay (
                         VStack(spacing: 6) {
-                            Text(targetSubGoal?.title ?? "No SubGoal")
-                                .foregroundStyle(.my538F53)
+                            Text(viewModel.targetSubGoal?.title ?? "No SubGoal")
                                 .font(.Pretendard.Medium.size16)
-                            Text(targetDetailGoal?.title ?? "No DetailGoal")
+                                .foregroundStyle(.my538F53)
+                            Text(viewModel.targetDetailGoal?.title ?? "No DetailGoal")
                                 .font(.Pretendard.Medium.size20)
                         }
                     )
                     .padding(.top, 32)
                 
-                VStack(spacing: 2) {
-                    HStack {
-                        Text("요일 선택")
-                            .font(.Pretendard.SemiBold.size17)
-                            .foregroundStyle(.my675542)
-                            .kerning(0.2)
-                        Spacer()
-                    }
-                    HStack {
-                        Text("루틴을 실행할 요일을 선택해주세요 " )
-                            .font(.Pretendard.Medium.size14)
-                            .foregroundStyle(.myB4A99D)
-                            .kerning(0.2)
-                        Spacer()
-                    }
-                }
-                .padding(.leading, 4)
-                .padding(.top, 32)
+                selectDayView() // 요일 선택 뷰
                 
-                VStack(spacing: 0) {
-                    HStack {
-                        Text("반복 요일")
-                            .font(.Pretendard.SemiBold.size16)
-                            .kerning(0.2)
-                            .padding([.leading, .top], 16)
-                        Spacer()
-                    }
-                    
-                    HStack(spacing: 7) {
-                        DaysCycleCellButton(day: "일", isSelected: $alertSun, onChange: updateAchieveGoal)
-                        DaysCycleCellButton(day: "월", isSelected: $alertMon, onChange: updateAchieveGoal)
-                        DaysCycleCellButton(day: "화", isSelected: $alertTue, onChange: updateAchieveGoal)
-                        DaysCycleCellButton(day: "수", isSelected: $alertWed, onChange: updateAchieveGoal)
-                        DaysCycleCellButton(day: "목", isSelected: $alertThu, onChange: updateAchieveGoal)
-                        DaysCycleCellButton(day: "금", isSelected: $alertFri, onChange: updateAchieveGoal)
-                        DaysCycleCellButton(day: "토", isSelected: $alertSat, onChange: updateAchieveGoal)
-                    }
-                    .padding(.horizontal, 20)
-                    .padding(.top, 10)
-                    .padding(.bottom, 15)
-                }
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.myB4A99D, lineWidth: 1)
-                )
-                .cornerRadius(12)
-                .padding(.top, 10)
-                
-                VStack(spacing: 2) {
-                    HStack {
-                        Text("시간대 선택")
-                            .font(.Pretendard.SemiBold.size17)
-                            .foregroundStyle(.my675542)
-                            .kerning(0.2)
-                        Spacer()
-                    }
-                    HStack {
-                        Text("루틴을 실행할 대략적인 시간대를 선택해주세요. " )
-                            .font(.Pretendard.Medium.size14)
-                            .foregroundStyle(.myB4A99D)
-                            .kerning(0.2)
-                        Spacer()
-                    }
-                }
-                .padding(.leading, 4)
-                .padding(.top, 28)
-                
-                VStack(spacing: 0) {
-                    HStack {
-                        Text("루틴 시간대")
-                            .font(.Pretendard.SemiBold.size16)
-                            .kerning(0.2)
-                        Spacer()
-                        Picker("Choose a times", selection: $selectedTime) {
-                            ForEach(routineTimes, id: \.self) {
-                                Text($0)
-                                    .font(.Pretendard.Regular.size17)
-                            }
-                        }
-                        .pickerStyle(.menu)
-                        .tint(.my89898D)
-                    }
-                    .padding(.vertical)
-                    .padding(.horizontal,16)
-                }
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.myB4A99D, lineWidth: 1)
-                )
-                .cornerRadius(12)
-                .padding(.top, 10)
-                .padding(.bottom, 32)
+                selectTimesView() // 시간대 선택 뷰
             }
-            
-            NextButton(isEnabled: achieveGoal > 0 ) {
-                achieveGoal = [alertMon, alertTue, alertWed, alertThu, alertFri, alertSat, alertSun]
-                    .filter { $0 }
-                    .count
-                
-                guard let targetDetailGoal = targetDetailGoal else {
-                    print("⚠️ targetDetailGoal is nil")
-                    return
-                }
-                
-                // Picker에서 선택한 시간대에 따라 업데이트
-                let isAfternoon = selectedTime == "점심"
-                let isEvening = selectedTime == "저녁"
-                let isNight = selectedTime == "자기 전"
-                let isFree = selectedTime == "자율"
-                
-                // isMorning 업데이트 조건 추가
-                let isMorning: Bool
-                if isAfternoon || isEvening || isNight || isFree {
-                    isMorning = false // 다른 시간대가 선택된 경우 false
-                } else {
-                    isMorning = targetDetailGoal.isMorning // 아무것도 선택되지 않은 경우 기존 값 유지
-                }
-                viewModel.updateDetailGoal(
-                    detailGoal: targetDetailGoal,
-                    newTitle: targetDetailGoal.title,
-                    newMemo: targetDetailGoal.memo,
-                    achieveCount: targetDetailGoal.achieveCount,
-                    achieveGoal: achieveGoal,
-                    alertMon: alertMon,
-                    alertTue: alertTue,
-                    alertWed: alertWed,
-                    alertThu: alertThu,
-                    alertFri: alertFri,
-                    alertSat: alertSat,
-                    alertSun: alertSun,
-                    isRemind: targetDetailGoal.isRemind,
-                    remindTime: targetDetailGoal.remindTime,
-                    achieveMon: targetDetailGoal.achieveMon,
-                    achieveTue: targetDetailGoal.achieveTue,
-                    achieveWed: targetDetailGoal.achieveWed,
-                    achieveThu: targetDetailGoal.achieveThu,
-                    achieveFri: targetDetailGoal.achieveFri,
-                    achieveSat: targetDetailGoal.achieveSat,
-                    achieveSun: targetDetailGoal.achieveSun,
-                    isMorning: isMorning,
-                    isAfternoon: isAfternoon,
-                    isEvening: isEvening,
-                    isNight: isNight,
-                    isFree: isFree
-                )
+            NextButton(isEnabled: viewModel.achieveGoal > 0 ) {
+                viewModel.performUpdateDetailGoal()
                 navigationManager.push(to: .onboardComplete)
             } label: {
                 Text("다음")
@@ -220,16 +63,109 @@ struct DaysCycleView: View {
         .background(.myFFFAF4)
         .onAppear {
             // 사용자가 입력한 Subgoal id 1의 값을 targetSubGoal에 저장
-            targetSubGoal = subGoals.first(where: { $0.id == 1 })
+            viewModel.targetSubGoal = subGoals.first(where: { $0.id == 1 })
             // targetSubGoal의 DetailGoal 중 id 1의 값을 targetDetailGoal에 저장
-            targetDetailGoal = targetSubGoal?.detailGoals.first(where: { $0.id == 1 })
+            viewModel.targetDetailGoal = viewModel.targetSubGoal?.detailGoals.first(where: { $0.id == 1 })
         }
     }
     
-    private func updateAchieveGoal() {
-        achieveGoal = [alertMon, alertTue, alertWed, alertThu, alertFri, alertSat, alertSun]
-            .filter { $0 }
-            .count
+    @ViewBuilder
+    private func selectDayView() -> some View {
+        VStack(spacing: 2) {
+            HStack {
+                Text("요일 선택")
+                    .font(.Pretendard.SemiBold.size17)
+                    .foregroundStyle(.my675542)
+                    .kerning(0.2)
+                Spacer()
+            }
+            HStack {
+                Text("루틴을 실행할 요일을 선택해주세요 " )
+                    .font(.Pretendard.Medium.size14)
+                    .foregroundStyle(.myB4A99D)
+                    .kerning(0.2)
+                Spacer()
+            }
+        }
+        .padding(.leading, 4)
+        .padding(.top, 32)
+        
+        VStack(spacing: 0) {
+            HStack {
+                Text("반복 요일")
+                    .font(.Pretendard.SemiBold.size16)
+                    .kerning(0.2)
+                    .padding([.leading, .top], 16)
+                Spacer()
+            }
+            
+            HStack(spacing: 7) {
+                DaysCycleCellButton(day: "일", isSelected: $viewModel.alertSun, onChange: viewModel.updateAchieveGoal)
+                DaysCycleCellButton(day: "월", isSelected: $viewModel.alertMon, onChange: viewModel.updateAchieveGoal)
+                DaysCycleCellButton(day: "화", isSelected: $viewModel.alertTue, onChange: viewModel.updateAchieveGoal)
+                DaysCycleCellButton(day: "수", isSelected: $viewModel.alertWed, onChange: viewModel.updateAchieveGoal)
+                DaysCycleCellButton(day: "목", isSelected: $viewModel.alertThu, onChange: viewModel.updateAchieveGoal)
+                DaysCycleCellButton(day: "금", isSelected: $viewModel.alertFri, onChange: viewModel.updateAchieveGoal)
+                DaysCycleCellButton(day: "토", isSelected: $viewModel.alertSat, onChange: viewModel.updateAchieveGoal)
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 10)
+            .padding(.bottom, 15)
+        }
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.myB4A99D, lineWidth: 1)
+        )
+        .cornerRadius(12)
+        .padding(.top, 10)
+    }
+    
+    @ViewBuilder
+    private func selectTimesView() -> some View {
+        VStack(spacing: 2) {
+            HStack {
+                Text("시간대 선택")
+                    .font(.Pretendard.SemiBold.size17)
+                    .foregroundStyle(.my675542)
+                    .kerning(0.2)
+                Spacer()
+            }
+            HStack {
+                Text("루틴을 실행할 대략적인 시간대를 선택해주세요. " )
+                    .font(.Pretendard.Medium.size14)
+                    .foregroundStyle(.myB4A99D)
+                    .kerning(0.2)
+                Spacer()
+            }
+        }
+        .padding(.leading, 4)
+        .padding(.top, 28)
+        
+        VStack(spacing: 0) {
+            HStack {
+                Text("루틴 시간대")
+                    .font(.Pretendard.SemiBold.size16)
+                    .kerning(0.2)
+                Spacer()
+                Picker("Choose a times", selection: $viewModel.selectedTime) {
+                    ForEach(viewModel.routineTimes, id: \.self) {
+                        Text($0)
+                            .font(.Pretendard.Regular.size17)
+                    }
+                }
+                .pickerStyle(.menu)
+                .tint(.my89898D)
+            }
+            .padding(.vertical)
+            .padding(.horizontal,16)
+        }
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.myB4A99D, lineWidth: 1)
+        )
+        .cornerRadius(12)
+        .padding(.top, 10)
+        .padding(.bottom, 32)
     }
 }
 
--- OneByte/Source/Features/Onboarding/ViewModel/RoutineCycleViewModel.swift
+++ OneByte/Source/Features/Onboarding/ViewModel/RoutineCycleViewModel.swift
@@ -20,9 +20,21 @@ class RoutineCycleViewModel {
     var userNewDetailGoal: String = "" // 사용자 SubGoal 입력 텍스트
     let detailGoalLimit = 20 // DetailGoal 글자 수 제한
     
+    // DaysCycleView
+    var targetDetailGoal: DetailGoal?
+    var achieveGoal = 0
+    var alertMon = false
+    var alertTue = false
+    var alertWed = false
+    var alertThu = false
+    var alertFri = false
+    var alertSat = false
+    var alertSun = false
+    var selectedTime = ""
+    var routineTimes = ["아침","점심","저녁","자기 전","자율"]
+    
     var navigationManager = NavigationManager()
     private let updateService: UpdateGoalUseCase
-    
     init(updateService: UpdateGoalUseCase) {
         self.updateService = updateService
     }
@@ -40,6 +52,70 @@ class RoutineCycleViewModel {
     func updateDetailGoal(detailGoal: DetailGoal, newTitle: String, newMemo: String, achieveCount: Int, achieveGoal: Int, alertMon: Bool, alertTue: Bool, alertWed: Bool, alertThu: Bool, alertFri: Bool, alertSat: Bool, alertSun: Bool, isRemind: Bool, remindTime: Date?, achieveMon: Bool, achieveTue: Bool, achieveWed: Bool, achieveThu: Bool, achieveFri: Bool, achieveSat: Bool, achieveSun: Bool, isMorning: Bool, isAfternoon: Bool, isEvening: Bool, isNight: Bool, isFree: Bool) {
         updateService.updateDetailGoal(detailGoal: detailGoal, title: newTitle, memo: newMemo, achieveCount: achieveCount, achieveGoal: achieveGoal, alertMon: alertMon, alertTue: alertTue, alertWed: alertWed, alertThu: alertThu, alertFri: alertFri, alertSat: alertSat, alertSun: alertSun, isRemind: isRemind, remindTime: remindTime, achieveMon: achieveMon, achieveTue: achieveTue, achieveWed: achieveWed, achieveThu: achieveThu, achieveFri: achieveFri, achieveSat: achieveFri, achieveSun: achieveSun, isMorning: isMorning, isAfternoon: isAfternoon, isEvening: isEvening, isNight: isNight, isFree: isFree)
         
+    }
+    
+    // 요일 선택에 따른 `achieveGoal` 계산
+    func updateAchieveGoal() {
+        achieveGoal = [alertMon, alertTue, alertWed, alertThu, alertFri, alertSat, alertSun]
+            .filter { $0 }
+            .count
+    }
+    
+    // 시간대 선택에 따른 업데이트 조건 계산
+    func calculateTimeUpdateConditions() -> (isMorning: Bool, isAfternoon: Bool, isEvening: Bool, isNight: Bool, isFree: Bool) {
+        let isAfternoon = selectedTime == "점심"
+        let isEvening = selectedTime == "저녁"
+        let isNight = selectedTime == "자기 전"
+        let isFree = selectedTime == "자율"
+        
+        let isMorning: Bool
+        if isAfternoon || isEvening || isNight || isFree {
+            isMorning = false // 다른 시간대가 선택된 경우 false
+        } else {
+            isMorning = targetDetailGoal?.isMorning ?? false // 아무것도 선택되지 않은 경우 기존 값 유지
+        }
+        
+        return (isMorning, isAfternoon, isEvening, isNight, isFree)
+    }
+    
+    // 요일 + 시간대 선택 후 DetailGoal 업데이트
+    func performUpdateDetailGoal() {
+        guard let targetDetailGoal = targetDetailGoal else {
+            print("⚠️ targetDetailGoal is nil")
+            return
+        }
+        // 시간대 조건 계산
+        let (isMorning, isAfternoon, isEvening, isNight, isFree) = calculateTimeUpdateConditions()
+        
+        // 업데이트 호출
+        updateDetailGoal(
+            detailGoal: targetDetailGoal,
+            newTitle: targetDetailGoal.title,
+            newMemo: targetDetailGoal.memo,
+            achieveCount: targetDetailGoal.achieveCount,
+            achieveGoal: achieveGoal,
+            alertMon: alertMon,
+            alertTue: alertTue,
+            alertWed: alertWed,
+            alertThu: alertThu,
+            alertFri: alertFri,
+            alertSat: alertSat,
+            alertSun: alertSun,
+            isRemind: targetDetailGoal.isRemind,
+            remindTime: targetDetailGoal.remindTime,
+            achieveMon: targetDetailGoal.achieveMon,
+            achieveTue: targetDetailGoal.achieveTue,
+            achieveWed: targetDetailGoal.achieveWed,
+            achieveThu: targetDetailGoal.achieveThu,
+            achieveFri: targetDetailGoal.achieveFri,
+            achieveSat: targetDetailGoal.achieveSat,
+            achieveSun: targetDetailGoal.achieveSun,
+            isMorning: isMorning,
+            isAfternoon: isAfternoon,
+            isEvening: isEvening,
+            isNight: isNight,
+            isFree: isFree
+        )
     }
     
     // 사용자 입력 DetailGoal 비우기
