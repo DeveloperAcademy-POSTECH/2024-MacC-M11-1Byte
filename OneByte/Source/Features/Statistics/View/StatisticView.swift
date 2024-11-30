ffe76206546a7079e9158200744730c38a32e622
Dobbinci <davinlsj@gmail.com>
2024-12-01 01:23:33 +0900

    Feat:  thisYearCloverInfo view 삭제

--- OneByte/Source/Features/Statistics/View/StatisticView.swift
+++ OneByte/Source/Features/Statistics/View/StatisticView.swift
@@ -41,10 +41,9 @@ struct StatisticView: View {
                     ScrollView {
                         thisMonthCloverInfoView()
                             .padding(.top, 44)
-                        thisYearCloverInfoView()
-                            .padding(.top, 32)
+
                         weeklyCloverInfoView()
-                            .padding(.top, 10)
+                            .padding(.top, 21)
                         
                         Spacer()
                     }
@@ -114,12 +113,12 @@ struct StatisticView: View {
         }
     }
     
-    
-    
     @ViewBuilder
-    private func thisYearCloverInfoView() -> some View {
-        VStack(spacing: 10){
-            VStack(spacing: 2) {
+    private func weeklyCloverInfoView() -> some View {
+        @State var rectangleHeight = viewModel.weeklyCloverInfoHeight
+        
+        VStack(spacing: 12) {
+            VStack(spacing: 4) {
                 HStack {
                     Text("올해의 클로버")
                         .font(.Pretendard.SemiBold.size18)
@@ -127,155 +126,143 @@ struct StatisticView: View {
                     Spacer()
                 }
                 HStack {
-                    Text("올해의 내가 수집한 클로버를 모아볼 수 있어요.")
+                    Text("올해의 내가 수집한 클로버를 모아볼 수 있어요")
                         .font(.Pretendard.Medium.size14)
-                        .foregroundStyle(Color.myA8A8A8)
+                        .foregroundStyle(Color.my909090)
                         .padding(.leading, 5)
                     Spacer()
                 }
             }
-            
-            HStack(alignment: .center) {
-                VStack(spacing: 10) {
-                    HStack {
-                        Text("내가 모은 황금 클로버")
-                            .font(.Pretendard.Medium.size15)
-                        Spacer()
-                    }
-                    HStack {
-                        Image("Clover_Gold")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                        Text("\(viewModel.currentYearCloverStates[2])")
-                            .font(.Pretendard.SemiBold.size20)
-                        Spacer()
-                    }
-                }
-                .padding(.leading, 20)
+            ZStack(alignment: .top) {
+                Rectangle()
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: viewModel.weeklyCloverInfoHeight)
+                    .cornerRadius(13)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 13)
+                            .stroke(Color.myF0E8DF, lineWidth: 1)
+                    )
                 
-                Divider()
-                    .frame(height: 60)
-                    .background(Color.myD5D5D5)
-                
-                VStack(spacing: 10) {
-                    HStack {
-                        Text("내가 모은 클로버")
-                            .font(.Pretendard.Medium.size15)
-                        Spacer()
-                    }
-                    HStack {
-                        Image("Clover_Green")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                        Text("\(viewModel.currentYearCloverStates[1])")
-                            .font(.Pretendard.SemiBold.size20)
-                        Spacer()
-                    }
-                }
-                .padding(.leading, 20)
-            }
-            .frame(maxWidth: .infinity)
-            .frame(height: 82)
-            .background(.white)
-            .cornerRadius(12)
-            .overlay(
-                RoundedRectangle(cornerRadius: 12)
-                    .stroke(Color.myF0E8DF, lineWidth: 1)
-            )
-        }
-    }
-    
-    @ViewBuilder
-    private func weeklyCloverInfoView() -> some View {
-        @State var rectangleHeight = viewModel.weeklyCloverInfoHeight
-        ZStack(alignment: .top) {
-            Rectangle()
-                .foregroundStyle(.white)
-                .frame(maxWidth: .infinity)
-                .frame(height: viewModel.weeklyCloverInfoHeight)
-                .cornerRadius(13)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 13)
-                        .stroke(Color.myF0E8DF, lineWidth: 1)
-                )
-            
-            VStack(spacing: 0) {
-                if let range = viewModel.currentYearCloverMonthRange {
-                    HStack(spacing: 30) {
-                        Spacer()
-                            .frame(width: 20)
-                        Text("1주차")
-                            .font(Font.Pretendard.SemiBold.size12)
-                            .foregroundStyle(.my9C9C9C)
-                        Text("2주차")
-                            .font(Font.Pretendard.SemiBold.size12)
-                            .foregroundStyle(.my9C9C9C)
-                        Text("3주차")
-                            .font(Font.Pretendard.SemiBold.size12)
-                            .foregroundStyle(.my9C9C9C)
-                        Text("4주차")
-                            .font(Font.Pretendard.SemiBold.size12)
-                            .foregroundStyle(.my9C9C9C)
-                        Text("5주차")
-                            .font(Font.Pretendard.SemiBold.size12)
-                            .foregroundStyle(.my9C9C9C)
-                    }
-                    .padding(.top, 16)
-                    .padding(.bottom, 30)
-                    
-                    ForEach(Array(stride(from: viewModel.currentMonth, through: range.min, by: -1)), id: \.self) { month in // 내림차순
-                        let cloversForMonth = viewModel.filterCloversByMonth(clovers: viewModel.currentYearClovers, month: month)
-                        VStack(spacing: 0) {
-                            HStack(spacing: 20) {
-                                Text("\(month)월")
-                                    .font(Font.Pretendard.Bold.size14)
-                                    .foregroundStyle(.my566956)
-                                    .frame(width: 30)
+                VStack(spacing: 0) {
+                    HStack(alignment: .top, spacing: 0) {
+                        VStack(spacing: 10) {
+                            HStack {
+                                Text("초록 클로버")
+                                    .font(.Pretendard.Medium.size14)
+                                    .foregroundStyle(Color.my909090)
                                 
-                                ForEach(1...5, id: \.self) { week in
-                                    let cloverForWeek = cloversForMonth.filter { $0.cloverWeekOfMonth == week }
-                                    
-                                    if let firstClover = cloverForWeek.first {
-                                        switch firstClover.cloverState {
-                                        case 0:
-                                            Image("Clover_Empty")
-                                                .resizable()
-                                                .frame(width: 38, height: 38)
-                                        case 1:
-                                            Image("Clover_Light")
-                                                .resizable()
-                                                .frame(width: 38, height: 38)
-                                        case 2:
-                                            Image("Clover_Green")
-                                                .resizable()
-                                                .frame(width: 38, height: 38)
-                                        case 3:
-                                            Image("Clover_Gold")
-                                                .resizable()
-                                                .frame(width: 38, height: 38)
-                                        default:
-                                            Image("Clover_Empty")
-                                                .resizable()
-                                                .frame(width: 38, height: 38)
-                                        }
-                                    } else {
-                                        Rectangle()
-                                            .fill(.clear)
-                                            .frame(width: 38, height: 38)
-                                    }
-                                }
                             }
-                            if viewModel.weeklyCloverInfoHeight > 100 && month != range.min {
-                                Divider()
-                                    .padding(12)
+                            HStack {
+                                Image("Clover_Green")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                Text("\(viewModel.currentYearCloverStates[1])개")
+                                    .font(.Pretendard.SemiBold.size16)
+                                
                             }
-                            
+                        }
+                        
+                        Divider()
+                            .background(Color.myD5D5D5)
+                            .padding(.horizontal, 59)
+                        
+                        VStack(spacing: 10) {
+                            HStack {
+                                Text("황금 클로버")
+                                    .font(.Pretendard.Medium.size14)
+                                    .foregroundStyle(Color.my909090)
+                                
+                            }
+                            HStack {
+                                Image("Clover_Gold")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                Text("\(viewModel.currentYearCloverStates[2])개")
+                                    .font(.Pretendard.SemiBold.size16)
+                                
+                            }
                         }
                     }
-                } else {
-                    Text("클로버를 찾을 수 없습니다")
-                        .font(.Pretendard.Regular.size16)
-                        .padding(.top, 12)
+                    .padding(.top, 20)
+                    
+                    if let range = viewModel.currentYearCloverMonthRange {
+                        HStack(spacing: 30) {
+                            Spacer()
+                                .frame(width: 20)
+                            Text("1주차")
+                                .font(Font.Pretendard.SemiBold.size12)
+                                .foregroundStyle(.my9C9C9C)
+                            Text("2주차")
+                                .font(Font.Pretendard.SemiBold.size12)
+                                .foregroundStyle(.my9C9C9C)
+                            Text("3주차")
+                                .font(Font.Pretendard.SemiBold.size12)
+                                .foregroundStyle(.my9C9C9C)
+                            Text("4주차")
+                                .font(Font.Pretendard.SemiBold.size12)
+                                .foregroundStyle(.my9C9C9C)
+                            Text("5주차")
+                                .font(Font.Pretendard.SemiBold.size12)
+                                .foregroundStyle(.my9C9C9C)
+                        }
+                        .padding(.top, 32)
+                        .padding(.bottom, 30)
+                        
+                        ForEach(Array(stride(from: viewModel.currentMonth, through: range.min, by: -1)), id: \.self) { month in // 내림차순
+                            let cloversForMonth = viewModel.filterCloversByMonth(clovers: viewModel.currentYearClovers, month: month)
+                            VStack(spacing: 0) {
+                                HStack(spacing: 20) {
+                                    Text("\(month)월")
+                                        .font(Font.Pretendard.Bold.size14)
+                                        .foregroundStyle(.my566956)
+                                        .frame(width: 30)
+                                    
+                                    ForEach(1...5, id: \.self) { week in
+                                        let cloverForWeek = cloversForMonth.filter { $0.cloverWeekOfMonth == week }
+                                        
+                                        if let firstClover = cloverForWeek.first {
+                                            switch firstClover.cloverState {
+                                            case 0:
+                                                Image("Clover_Empty")
+                                                    .resizable()
+                                                    .frame(width: 38, height: 38)
+                                            case 1:
+                                                Image("Clover_Light")
+                                                    .resizable()
+                                                    .frame(width: 38, height: 38)
+                                            case 2:
+                                                Image("Clover_Green")
+                                                    .resizable()
+                                                    .frame(width: 38, height: 38)
+                                            case 3:
+                                                Image("Clover_Gold")
+                                                    .resizable()
+                                                    .frame(width: 38, height: 38)
+                                            default:
+                                                Image("Clover_Empty")
+                                                    .resizable()
+                                                    .frame(width: 38, height: 38)
+                                            }
+                                        } else {
+                                            Rectangle()
+                                                .fill(.clear)
+                                                .frame(width: 38, height: 38)
+                                        }
+                                    }
+                                }
+                                if viewModel.weeklyCloverInfoHeight > 100 && month != range.min {
+                                    Divider()
+                                        .padding(12)
+                                }
+                                
+                            }
+                        }
+                    } else {
+                        Text("클로버를 찾을 수 없습니다")
+                            .font(.Pretendard.Regular.size16)
+                            .padding(.top, 12)
+                    }
                 }
             }
         }
