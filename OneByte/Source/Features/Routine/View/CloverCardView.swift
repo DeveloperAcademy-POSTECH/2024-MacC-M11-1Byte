0a97b8dabc4a4b08f8e2a6f02e2b824c32f28232
SANGDOLEE <xkdxh625@naver.com>
2024-11-30 16:58:37 +0900

    Feat: 완수율 통계 로드시, 자동으로 바텀쪽으로 스크롤
    ScrollViewReader 추가

--- OneByte/Source/Features/Routine/View/CloverCardView.swift
+++ OneByte/Source/Features/Routine/View/CloverCardView.swift
@@ -37,85 +37,96 @@ struct CloverCardView: View {
             .padding(.leading, 16)
             .padding(.bottom, 11)
             
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack(spacing: 3) {
-                    Text(cloverCardType.cloverCardTitle) // 클로버 종류
-                        .font(.Pretendard.Bold.size24)
-                        .foregroundStyle(.white)
-                    Text(cloverCardType.cloverCardMessage) // 메세지
-                        .font(.Pretendard.Medium.size16)
-                        .foregroundStyle(.white.opacity(0.9))
-                }
-                .padding(.top, 32)
-                
-                ZStack {
-                    Image(cloverCardType.cloverCardBackground) // 카드 배경
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 279, height: 360)
+            ScrollViewReader { proxy in
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack(spacing: 3) {
+                        Text(cloverCardType.cloverCardTitle) // 클로버 종류
+                            .font(.Pretendard.Bold.size24)
+                            .foregroundStyle(.white)
+                        Text(cloverCardType.cloverCardMessage) // 메세지
+                            .font(.Pretendard.Medium.size16)
+                            .foregroundStyle(.white.opacity(0.9))
+                    }
+                    .padding(.top, 32)
                     
-                    VStack {
-                        VStack(spacing: 2) {
-                            Text(viewModel.getLastWeekWeekofMonth()) // 이전 주차
-                                .font(.Pretendard.Bold.size18)
-                                .foregroundStyle(cloverCardType.cloverLastWeekDateColor)
-                            Text(cloverCardType.cloverType)
-                                .font(.Pretendard.ExtraBold.size24)
-                                .foregroundStyle(.white)
+                    ZStack {
+                        Image(cloverCardType.cloverCardBackground) // 카드 배경
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 279, height: 360)
+                        
+                        VStack {
+                            VStack(spacing: 2) {
+                                Text(viewModel.getLastWeekWeekofMonth()) // 이전 주차
+                                    .font(.Pretendard.Bold.size18)
+                                    .foregroundStyle(cloverCardType.cloverLastWeekDateColor)
+                                Text(cloverCardType.cloverType)
+                                    .font(.Pretendard.ExtraBold.size24)
+                                    .foregroundStyle(.white)
+                            }
+                            .padding(.top, 46)
+                            
+                            Image(cloverCardType.cloverCardClover ?? "") // 클로버 아이콘
+                                .rotation3DEffect (
+                                    .degrees(viewModel.rotationAngle),
+                                    axis: (x: 0, y: 1, z: 0)
+                                )
+                                .onAppear {
+                                    viewModel.startRotationAnimation() // 자동 회전
+                                }
+                                .onTapGesture {
+                                    viewModel.tapRotationAnimation() // 탭 회전
+                                }
+                                .padding(.top, 30)
+                            
+                            Spacer()
                         }
-                        .padding(.top, 46)
-                        
-                        Image(cloverCardType.cloverCardClover ?? "") // 클로버 아이콘
-                            .rotation3DEffect(
-                                .degrees(viewModel.rotationAngle),
-                                axis: (x: 0, y: 1, z: 0)
-                            )
-                            .onAppear {
-                                viewModel.startRotationAnimation() // 자동 회전
-                            }
-                            .onTapGesture {
-                                viewModel.tapRotationAnimation() // 탭 회전
-                            }
-                            .padding(.top, 30)
-                        
-                        Spacer()
                     }
-                }
-                .padding(.top, 39)
-                .frame(width: 279, height: 360)
-                
-                VStack(spacing: 6) {
+                    .padding(.top, 39)
+                    .frame(width: 279, height: 360)
+                    
+                    VStack(spacing: 6) {
+                        if viewModel.isCheckAchievement {
+                            Image(systemName: "chevron.up")
+                                .frame(width: 19, height: 11)
+                                .foregroundStyle(.white)
+                                .bold()
+                            Text("완수율 확인하기")
+                                .font(.Pretendard.Bold.size18)
+                                .foregroundStyle(.white)
+                                .onTapGesture {
+                                    if viewModel.isCheckAchievement {
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                            withAnimation {
+                                                proxy.scrollTo("scrollToBottom", anchor: .bottom)
+                                            }
+                                        }
+                                    }
+                                }
+                        } else {
+                            Text("루틴 완수율 확인하기")
+                                .font(.Pretendard.Bold.size18)
+                                .foregroundStyle(.white)
+                            Image(systemName: "chevron.down")
+                                .frame(width: 19, height: 11)
+                                .foregroundStyle(.white)
+                                .bold()
+                        }
+                    }
+                    .padding(.top, 24)
+                    .onTapGesture {
+                        viewModel.isCheckAchievement.toggle()
+                    }
+                    // 완수율 통계
                     if viewModel.isCheckAchievement {
-                        Image(systemName: "chevron.up")
-                            .frame(width: 19, height: 11)
-                            .foregroundStyle(.white)
-                            .bold()
-                        Text("완수율 확인하기")
-                            .font(.Pretendard.Bold.size18)
-                            .foregroundStyle(.white)
-                    } else {
-                        Text("루틴 완수율 확인하기")
-                            .font(.Pretendard.Bold.size18)
-                            .foregroundStyle(.white)
-                        Image(systemName: "chevron.down")
-                            .frame(width: 19, height: 11)
-                            .foregroundStyle(.white)
-                            .bold()
+                        completionRateView(cloverCardType: cloverCardType)
+                            .transition(.move(edge: .bottom))
+                            .id("scrollToBottom")
                     }
+                    Spacer()
                 }
-                .padding(.top, 24)
-                .onTapGesture {
-                    viewModel.isCheckAchievement.toggle()
-                }
-                
-                // 완수율 통계
-                if viewModel.isCheckAchievement {
-                    completionRateView(cloverCardType: cloverCardType)
-                        .transition(.move(edge: .bottom))
-                }
-                
-                Spacer()
             }
+            
             Button {
                 // action 뷰이동
             } label: {
